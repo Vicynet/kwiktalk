0016b3cb433e198cd4576d36a6c55413c6313147
Victor Ihedioha <ihedioha.victor@gmail.com>
2020-09-05 20:19:03 +0100

    Changed media url to media

--- account/urls.py
+++ account/urls.py
@@ -2,8 +2,6 @@ from django.urls import path, reverse_lazy
 from django.contrib.auth import views as auth_views
 from . import views
 
-app_name = 'account'
-
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     # path('login/', auth_views.LoginView.as_view(), name='login'),
